923d8c8666fe3bd9fb5c9aea0ec1769f6190bbc5
90-Ashraf <ashraf.90.issa@gmail.com>
2026-02-13 12:53:56 +0300

    First Create The Project

---
+++ InfluX/InfluX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InfluX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
